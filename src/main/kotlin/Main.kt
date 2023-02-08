a225c7711abef82f01a5e46d96566627919dd4bc
MrKearns <jkearns631@students.mchenry.edu>
2023-02-08 15:54:17 -0600

    first

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,63 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+// ---------- BUSINESS CONTACT ----------
+fun addContact(business: String, contact: String, phone: String  ): String {
+
+    var confirmation = ("$business\n$contact\n$phone")
+    println(confirmation)
+
+    return confirmation
+
+
+}
+
+// ---------- RESIDENTIAL CONTACT ----------
+fun addContact(name: String, phone: String): String {
+
+//    var name = readln().toString()
+//
+//    var phone = readln().toString()
+
+    var confirmation = ("$name\n$phone")
+    println(confirmation)
+
+    return confirmation
+
+}
+
+
+// ---------- MAIN ----------
+fun main(args: Array<String>?) {
+    println("Choose type of contact: ")
+    println("1: Business")
+    println("2: Residential")
+
+    var choice = readln().toInt()
+
+    when (choice){
+        1 -> {
+            println("Enter buisness name: ")
+            var business = readln().toString()
+            println("Enter Contact Name: ")
+            var contact = readln().toString()
+            println("Enter Phone Number: ")
+            var phone = readln().toString()
+
+            addContact(business, contact, phone)
+        }
+
+        2 -> {
+            println("Enter Your Name: ")
+            var name = readln().toString()
+            println("Enter Phone Number: ")
+            var phone = readln().toString()
+            addContact(name, phone)
+        }
+
+        else -> {
+            println("Invalid Choice")
+            main(null)
+        }
+    }
+
 }
